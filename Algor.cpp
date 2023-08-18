f292a402330420a2e64e12d6f9f0110f0e6e3846
FernandezMiguelDemetrius <124026957+FernandezMiguelDemetrius@users.noreply.github.com>
2023-08-18 08:45:29 +0800

    Add files via upload

---
+++ Algor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+	double length, width;
+	std:: cout << "Enter length: ";
+	std:: cin >> length;
+	 
+		std:: cout << "Enter width: ";
+		std:: cin >> width;
+		
+	double area = length*width;
+	 std:: cout << "The are of Rectangle is: "<< area << std::endl;
+	 
+return 0;	 
+}
